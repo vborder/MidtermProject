53c2b3306c0e87bf2a1179df8b7cb73a5e4f7c4b
Vincent Borden <vinbor81@gmail.com>
2020-05-21 13:28:46 -0400

    Stub Boot/MVC project

---
+++ BruShr/src/main/java/com/skilldistillery/brushr/BruShrApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.brushr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruShrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BruShrApplication.class, args);
+	}
+
+}
---
+++ BruShr/src/test/java/com/skilldistillery/brushr/BruShrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.brushr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruShrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
