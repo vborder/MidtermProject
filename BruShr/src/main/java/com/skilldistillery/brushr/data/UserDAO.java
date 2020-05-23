f7a9b0d18e2ae2c851e403649621f7198dce1ff6
darinthompson <darint07@gmail.com>
2020-05-22 19:08:35 -0600

    added and fixed the dao

--- BruShr/src/main/java/com/skilldistillery/brushr/data/UserDAO.java
+++ BruShr/src/main/java/com/skilldistillery/brushr/data/UserDAO.java
@@ -2,6 +2,7 @@ package com.skilldistillery.brushr.data;
 
 public interface UserDAO {
 	public void createUser(User user);
+	public User findUserByUserName(String name);
 	public User updateUser(int id, User user);
 	public boolean deleteUser(User user);
 }
