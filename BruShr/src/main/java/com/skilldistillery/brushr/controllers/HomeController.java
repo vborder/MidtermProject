ed3546321002f46e3e0bfb357965d951fbb27eae
Vincent Borden <vinbor81@gmail.com>
2020-05-27 18:58:42 -0400

    cleaned up home controller updateBeer methods

--- BruShr/src/main/java/com/skilldistillery/brushr/controllers/HomeController.java
+++ BruShr/src/main/java/com/skilldistillery/brushr/controllers/HomeController.java
@@ -125,18 +125,10 @@ public class HomeController {
 
 	@RequestMapping(path = "updateBeer.do", method = RequestMethod.POST)
 	public String updateBeerItem(@RequestParam(name = "id") int id, BeerRecipe beer, Model model) {
-//		BeerRecipe beer = beerDAO.getBeerById(id);
 		beerDAO.updateBeer(id, beer);
-//		model.addAttribute("beer", beer);
 		return "success";
 	}
-	
-//	BeerRecipe beerRecipe, HttpSession session, Model model) {
-//
-//		BeerRecipe beer = beerDAO.createBeer(beerRecipe, (User) session.getAttribute("user"));
-//
-//		model.addAttribute("beer", beer);
-	
+
 	/////// deleteBeer/////////////
 
 	@RequestMapping(path="deleteBeer.do")
@@ -144,9 +136,8 @@ public class HomeController {
 
 		BeerRecipe b = beerDAO.getBeerById(id);
 		User loggedInUser = (User) session.getAttribute("user");
-//		
 		boolean beerDelete = beerDAO.deleteBeer(id, loggedInUser);
-		System.out.println(id + "" + user);
+		
 		if (beerDelete) {
 			return "success";
 		}
@@ -162,7 +153,7 @@ public class HomeController {
 		model.addAttribute(b);
 		beerDAO.createComment(id, comment, user);
 		return getBeerById(id, model);
-  }
+	}
 	
 	@RequestMapping(path="about.do")
 	public String goToAboutPage() {
