5ff06d241f79c59e582a28a6bc71e6a4e5fb1c0f
darinthompson <darint07@gmail.com>
2020-05-27 15:59:13 -0600

    setup files for deployment to ec2'

--- BruShr/src/main/java/com/skilldistillery/brushr/BruShrApplication.java
+++ BruShr/src/main/java/com/skilldistillery/brushr/BruShrApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.brushr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BruShrApplication {
+public class BruShrApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BruShrApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BruShrApplication.class, args);
